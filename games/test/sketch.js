8e9b63d9487bf28d44dbcd4d97bc4d16ac1f34ba
= <=>
2017-09-14 15:50:01 -0400

    // Bug Fixes and Patches // -- fix play loading error -- added post style -- changed about.html

--- games/test/sketch.js
+++ games/test/sketch.js
@@ -2,11 +2,15 @@ function setup () {
 	// body...
 	var myCanvas = createCanvas(800, 600);
 	myCanvas.parent('game_container');
-	console.log('setup');
 }
 
 function draw () {
-	console.log('update');
-	background(0);
+	background(100);
+	testDraw();
 }
+
+// Import test
+console.log('Loaded sketch.js');
+//
+console.log('starting p5js');
 new p5();
---
+++ games/test/test.js
@@ -0,0 +1,8 @@
+// draw a picture thing
+function testDraw () {
+	fill(0);
+	text('poop', width/2, height/2);
+}
+
+// Import test
+console.log('Loaded test.js');
